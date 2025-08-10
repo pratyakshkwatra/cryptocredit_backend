6241be66b1e8ece3e1f4588a120a2696c63d730c
pratyakshkwatra <pratyakshkwatra@gmail.com>
2025-08-11 00:42:25 +0530

    bug-fix: minor bug fixes @ wallets.py; chore: update requirements.txt

--- routes/wallets.py
+++ routes/wallets.py
@@ -5,25 +5,27 @@ from models.wallet import Wallet
 from schemas import WalletCreate, WalletOut
 from auth_deps import get_current_user
 from models.user import User
-from utils import is_valid_address, is_supported_chain
+from utils import is_valid_address, can_fetch_data_from_goldrush
+from random_name import generate_name
 
 router = APIRouter(prefix="/wallets", tags=["Wallets"])
 
 @router.get("/", response_model=list[WalletOut])
 def get_wallets(
-    db: Session = Depends(get_db), current_user: User = Depends(get_current_user)
+    chain: str, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)
 ):
-    return db.query(Wallet).filter(Wallet.user_id == current_user.id).all()
+    return db.query(Wallet).filter(Wallet.user_id == current_user.id, Wallet.chain == chain).all()
 
 @router.post("/verify")
 def verify_wallet(wallet: WalletCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     if not is_valid_address(wallet.address):
-        raise HTTPException(status_code=400, detail="Invalid wallet address format.")
+        return {"message": "Invalid wallet address format.", "error": True}
 
-    if not is_supported_chain(wallet.chain):
-        raise HTTPException(status_code=400, detail="Unsupported or invalid chain.")
+    if not can_fetch_data_from_goldrush(wallet.address, wallet.chain):
+        return {"message": "Wallet address could not be verified on the specified chain.",  "error": True}
 
-    return {"message": "Wallet address and chain are valid."}
+    return {"message": "Wallet address and chain are valid and verified via goldrush.",  "error": False}
+
 
 @router.post("/", response_model=WalletOut)
 def add_wallet(
@@ -31,15 +33,17 @@ def add_wallet(
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
-
     if not is_valid_address(wallet.address):
         raise HTTPException(status_code=400, detail="Invalid wallet address format.")
-
-    if not is_supported_chain(wallet.chain):
-        raise HTTPException(status_code=400, detail="Unsupported or invalid chain.")
+    
+    if str(wallet.nickname).strip() != "":
+        nickname = str(wallet.nickname).strip()
+    else:
+        nickname = generate_name()
+    
 
     db_wallet = Wallet(
-        address=wallet.address, chain=wallet.chain, user_id=current_user.id
+        address=wallet.address, chain=wallet.chain, user_id=current_user.id, nickname=nickname
     )
     db.add(db_wallet)
     db.commit()
